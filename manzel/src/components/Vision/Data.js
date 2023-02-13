e019e7b9b6e3efdea47e10d777b81f831841e410
hadeel997 <hadeelmahmoud723@gmail.com>
2023-02-13 12:57:15 +0300

    Updating Our vision, about us

--- manzel/src/components/Vision/Data.js
+++ manzel/src/components/Vision/Data.js
@@ -6,7 +6,7 @@ export const visionObj = {
   topLine: "Goals, Goals, Goals",
   headLine: "Our Vision & Mission.",
   description:
-    "  Our Vision is to take education to the next level. Traditional teaching methods are: Good and beneficial to a certain point,lacking in the visualization of information, students (us included) were shocked by the vast difference between written words and the Real-world, so Manzel is our solution to this problem that will change education forever. Our mission impossible is to change all aspects of education, at all levels, from kids to experts, from zero to hero, at all aspects of life, handing knowledge on a silver glass (not dish because that would be weird!!). Provide better education to everyone, because great education is the core of a great society.",
+    "  aims to bridge universities and market jobs by transforming theoretical knowledge into a poignant interactive easy-to-learn experiment to build a solid foundation that helps students and engineers to gain more experiences before engaging with a real job in less time, Manzel has three main goals: Interactive and enjoyable content, Easy to learn, Accurate educational knowledge.",
   buttonLabel: "Get Started",
   imgStart: false,
   img: require("../../images/vision.svg"),
@@ -24,7 +24,7 @@ export const aboutObj = {
   topLine: "Wanna Know anything about us?",
   headLine: "About Us.",
   description:
-    "Manzel is the passion product of Three of the most creative, weird, wacky, and ambitious dreamers in the world. Our goal is to take education to the next level. With the help of Virtual Reality and Artificial intelligence, the first step is enhancing education for Architect Students and Electrical Engineering Students. because GOD HELP THEM! They really need help, but our good deed will not stop here; all education at all levels will be pumped up a notch.",
+    "Manzel is an Educational Platform in virtual reality. That specializes in making education more interactive by linking Architectural education with games and artificial intelligence to increase accuracy, with the highest quality of education and the shortest time possible for estimation.",
   buttonLabel: "Learn More",
   imgStart: true,
   img: require("../../images/about-us.svg"),
